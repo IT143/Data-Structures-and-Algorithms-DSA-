5e3774a8694b6d542dc998b28b3a7c9f0e43e4d7
Ishita Thoke <ishitathoke@gmail.com>
2026-02-13 01:43:52 +0530

    Add example of pass by value in C++
    Demonstrate pass by value in C++ with a function that modifies a copy of an integer.

---
+++ C++/Functions/Paramters.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void change(int num);
+
+int main() {
+  int x = 3;       // Original variable
+  change(x);      // x is copied into num
+  cout << "Pass by Value: " << x;   // Still prints 3
+}
+
+void change(int num) {    // This function gets a COPY of the number
+  num = 9;              // We change the copy only
+  cout << num << endl;  // Prints the updated copied number
+}
+
+// Not preferred for large-sized data structures, as it involves copying the value
+
+/*
+Pass by value means: When you send a variable to a function, the function gets a copy of it.
+If the function changes it, only the copy changes.
+The original variable stays the same.
+*/
