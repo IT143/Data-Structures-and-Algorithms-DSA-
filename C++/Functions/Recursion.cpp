8a0455ba2f9c9cd0973a9a2ce53b4d7be507a4ee
Ishita Thoke <ishitathoke@gmail.com>
2026-02-15 12:00:52 +0530

    Update Recursion.cpp

--- C++/Functions/Recursion.cpp
+++ C++/Functions/Recursion.cpp
@@ -7,10 +7,10 @@ int main() {
   helloWorld(5);     
 }
 
-void helloWorld (int n) {
-  if (n == 0) {
+void helloWorld (int n) {  // Recursive Function: A function which calls itself repeatedly until the base condition is met
+  if (n == 0) {    // Base Case (Stopping Condition) : Stops recursion when n == 0
     return;
   }
-  cout << "Hello World!" << endl;
-  helloWorld(n-1);
+  cout << "Hello World!" << endl;    // Prints Hello World before making the next call
+  helloWorld(n-1);  // Recursive Case: Calls the function with n-1
 }
