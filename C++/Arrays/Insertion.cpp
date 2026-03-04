53e3aea7ac607fc95d835508dfd09ba62ccf118f
Ishita Thoke <ishitathoke@gmail.com>
2026-03-04 23:46:02 +0530

    Refactor insertion functions and improve input handling

--- C++/Arrays/Insertion.cpp
+++ C++/Arrays/Insertion.cpp
@@ -1,79 +1,72 @@
 #include <iostream>
 using namespace std;
-#include <vector> // Compiler recognises vector, which is a dynamic array, i.e., it can shrink or expand
+#include <vector>
 
-
-// vector_name.push_back(value) - adds the element at the end of the vector and increases its size by 1
-
-vector<int> beginning(vector<int> &arr, int x);
-vector<int> position(vector<int> &arr, int x, int pos);
-vector<int> end(vector<int> &arr, int x);
-vector<int> display(vector<int> &arr);
+vector<int> beginning (vector<int> &arr, int x);
+vector<int> position (vector<int> &arr, int x, int pos);
+vector<int> end (vector<int> &arr, int x);
+vector<int> display (vector<int> &arr);
 
 int main() {
-    vector<int> arr = {10,20,30,40,50};  // Initializing an array
+    int x, ch;
+    vector<int> arr = {10,20,30,40,50,60};
+    cout << "\n1.At the beginning\n2.At a specific position\n3.At the end" << endl;
+    cout << "Enter the choice: ";
+    cin >> ch;
 
-    cout << "Before Insertion: ";
-    display(arr);
+    switch (ch) {
+        case 1: 
+        cout << "Enter the number to insert: ";
+        cin >> x;
+        beginning (arr, x);
+        display(arr);
+        break;
 
-    int x;
-    cout << "\nEnter the element to insert: ";
-    cin >> x;
+        case 2:
+        int pos;
+        cout << "Enter the number to insert: ";
+        cin >> x;
+        cout << "Enter the position: ";
+        cin >> pos;
+        position(arr, x, pos);
+        display(arr);
+        break;
 
-    beginning(arr, x);
-    display(arr);
-
-    end(arr, x);
-    display(arr);
-
-    int pos;
-    cout << "\nEnter the position: ";
-    cin >> pos;
-
-    position(arr, x, pos);
-    display(arr);
-
-    return 0;
-}
-
-vector<int> beginning(vector<int> &arr, int x) {
-    cout << "\nAfter inserting at the beginning: ";
-
-    arr.push_back(0);
-
-    for (int i = arr.size() - 1; i > 0; i--) {      // Each element moves right to make space at index 0.
-        arr[i] = arr[i-1];
+        case 3: 
+        cout << "Enter the number to insert: ";
+        cin >> x;
+        end (arr, x);
+        display(arr);
+        break;
     }
-
-    arr[0] = x;    // Insert new element at index 0
-    return arr;
 }
 
-vector<int> end(vector<int> &arr, int x) {
-    cout << "\nAfter inserting at the end: ";
-
-    arr.push_back(x);     // No shifting needed because end already has free expandable space
-
-    return arr;
-}
-
-vector<int> position(vector<int> &arr, int x, int pos) {
-    cout << "\nAfter inserting by position: ";
-
-    arr.push_back(0);
-
-    for (int i = arr.size() - 1; i > pos; i--) {    // Shift elements right from the position
-        arr[i] = arr[i-1];
+vector<int> beginning (vector<int> &arr, int x) {
+    arr.push_back(0);                                   // Adds zero to the end of the vector
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        arr[i] = arr[i-1];                        // All the elements shift to the right, creating space at index 0
     }
-
-    arr[pos] = x;
+    arr[0] = x;         // Inserts element x at index 0
     return arr;
 }
 
-vector<int> display(vector<int> &arr) {
+vector<int> position (vector<int> &arr, int x, int pos) {
+    arr.push_back(0);
+    for (int i = arr.size() - 1; i >= pos; i--) {        // Shifts to the right from the position we want to insert in
+        arr[i] = arr[i-1];    
+    }
+    arr[pos-1] = x;                // Inserts element at x at the position, not the index, therefore we reduce by 1
+    return arr
+}
+
+vector<int> end (vector<int> &arr, int x) {
+    arr.push_back(x);
+    return arr;
+}
+
+
+vector<int> display (vector<int> &arr) {
     for (int i = 0; i < arr.size(); i++) {
         cout << arr[i] << " ";
     }
-    cout << endl;
-    return arr;
 }
